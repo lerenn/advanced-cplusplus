94de557ac74de502c33eb7c62e5822e939d77048
Louis FRADIN <louis.fradin@gmail.com>
2020-04-08 15:36:19 +0200

    Binary Files: add exercice

---
+++ BinaryFiles/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#pragma pack(push, 1)
+struct Person {
+    char name[50];
+    int age;
+    double weight;
+};
+#pragma pack(pop)
+
+int main() {
+    cout << sizeof(struct Person) << endl;
+    return 0;
+}
