d259ddaee0d6a44d45777222d0c1d5c1f27741af
Louis FRADIN <louis.fradin@gmail.com>
2020-04-08 15:49:31 +0200

    Binary Files: continue exercice

--- BinaryFiles/main.cpp
+++ BinaryFiles/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -6,11 +7,40 @@ using namespace std;
 struct Person {
     char name[50];
     int age;
-    double weight;
+    double height;
 };
 #pragma pack(pop)
 
 int main() {
-    cout << sizeof(struct Person) << endl;
+    Person someone = {"Frodo", 220, 0.8};
+    string fileName = "BinaryFiles/test.bin";
+
+    /// Write binary file //////////////////////////////////
+    ofstream outputFile;
+
+    outputFile.open(fileName, ios::binary);
+
+    if (outputFile.is_open()) {
+        outputFile.write(reinterpret_cast<char *>(&someone), sizeof(Person));
+        outputFile.close();
+    } else {
+        cout << "Could not create file " << fileName << endl;
+    }
+
+    /// Read binary file //////////////////////////////////
+    Person someoneElse = {};
+    ifstream inputFile;
+
+    inputFile.open(fileName, ios::binary);
+
+    if (inputFile.is_open()) {
+        inputFile.read(reinterpret_cast<char *>(&someoneElse), sizeof(Person));
+        inputFile.close();
+    } else {
+        cout << "Could not create file " << fileName << endl;
+    }
+
+    cout << someoneElse.name << ", " << someoneElse.age << ", " << someoneElse.height << endl;
+
     return 0;
 }
